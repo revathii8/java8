5e5b71a45b28107aa436818d8f540657e7177f80
revathii8 <govurevathy@gmail.com>
2025-09-05 23:35:44 -0500

    first commit

---
+++ C529_If_Else_If_Block2.java
@@ -0,0 +1,27 @@
+package basicprograms;
+
+public class C529_If_Else_If_Block2 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		if(a==b)
+		{
+			System.out.println("1");
+		}
+		else if(a>b)
+		{
+			System.out.println("2");
+		}
+		else
+		{
+			System.out.println("3");
+		}
+		
+
+	}
+
+}
